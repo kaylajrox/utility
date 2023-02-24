mistake in unit conversion and area

thermo_kinetic_gas_calc_func.py
@@ -24,7 +24,7 @@ def convert_celsius_to_kelvin(T_C):
     """
     T_K = T_C + 273.15
     return T_K
-def avg_boltzmann_velocity(m_atomic,T_C=22):
+def avg_boltzmann_velocity(m_atomic,T_C=25):
     """
 
     :param m_atomic: atomic mass of the given substance
@@ -37,7 +37,7 @@ def avg_boltzmann_velocity(m_atomic,T_C=22):
     return v_avg
 
 
-def rms_boltzmann_velocity(m_atomic,T_C=22):
+def rms_boltzmann_velocity(m_atomic,T_C=25):
     """
 
     :param m_atomic: atomic mass of the given substance
@@ -48,4 +48,10 @@ def rms_boltzmann_velocity(m_atomic,T_C=22):
     m_kg = convert_atomic_mass_kg(m_atomic)
     v_rms = np.sqrt(3*kB*T/(pi*m_kg))
     return v_rms
+
+def volumetric_flow_rate(m_atomic,T_C=25):
+    T = convert_celsius_to_kelvin(T_C)
+    S = 1.455e4*np.sqrt(T/m_atomic)
+    return S
+
 

vacuum_calc.py
@@ -5,7 +5,10 @@ purely geometrical quantity that does not depend on pressure, but rather the typ
 
 """
 # TODO: make a database of all the atomic masses of the periodic table
-def conductance_tube(v,A,a=1):
+
+import numpy as np
+
+def conductance_aperture(v,A,a=1):
     """
 
     :param v: avg velocity of the gas, calculated using func in other file
@@ -14,4 +17,15 @@ def conductance_tube(v,A,a=1):
     :return C: conductance of the system in L/s
     """
     C = a*(v*A)/4
-    return C
+    return C*1000
+
+def conductance_circular_tube(v,d,l):
+    """
+    Conductance of a circular rounded tube calcuations
+    :param v: avg boltzmann velocity of a particle
+    :param d: diameter of the pipe
+    :param l: length of the pipe
+    :return: C_tube is conductance of the tube in L/s (multiply by 1000 for L)
+    """
+    C_tube = (np.pi)*v*d**3/(12*l)
+    return C_tube*1000