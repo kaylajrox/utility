21ea20b8b40b6f8f991772f4c1b71ff448fd8f27
Kayla <kaylajrox@gmail.com>
2023-03-23 15:45:54 -0400

    fix the plt show to properly show plot

--- trans_prob_plot.py
+++ trans_prob_plot.py
@@ -1,17 +1,21 @@
+"""
+Author: Kayla Rodriguez
+Purpose: Plot the transmission probability of various aspect ratios using vac tech data
+"""
 import pandas as pd
 import matplotlib.pyplot as plt
 
 # table 3.1 pg 36 in A User's Guide to Vacuum Technology
-trans_prob_round_tube_data_file = 'trans_prob_round_pipes.csv'
+TRANS_PROB_DATA_FILE = 'trans_prob_round_pipes.csv'
 
-data = pd.read_csv(trans_prob_round_tube_data_file)
+data = pd.read_csv(TRANS_PROB_DATA_FILE)
 
-last_points_del = 2
+LAST_POINTS_RM = 2
 
 all_aspect_ratios = data['l/d']
-aspect_ratios = all_aspect_ratios[: len(all_aspect_ratios)-last_points_del]
+aspect_ratios = all_aspect_ratios[: len(all_aspect_ratios)-LAST_POINTS_RM]
 trans_prob_all = data['a']
-trans_prob = trans_prob_all[: len(all_aspect_ratios)-last_points_del]
+trans_prob = trans_prob_all[: len(all_aspect_ratios)-LAST_POINTS_RM]
 
 fig = plt.figure()
 plt.ylabel("Transmission Probability a",fontsize=12)
@@ -19,4 +23,4 @@ plt.xlabel("Aspect ratio l/d",fontsize=12)
 plt.title("Transmission Probability for Round Pipes")
 plt.plot(aspect_ratios,trans_prob,'m.')
 plt.grid()
-fig.show()
+plt.show()
