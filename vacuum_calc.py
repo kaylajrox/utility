92b3d3ca3ccb8277722e5f920ff07f716e8d6a99
Kayla <kaylajrox@gmail.com>
2023-03-23 17:28:48 -0400

    calculation of particles being dumped into main chamber that need to be pumped out added to juptyer file

--- vacuum_calc.py
+++ vacuum_calc.py
@@ -5,16 +5,16 @@ Purpose: Provide functions for calculating conductances of various pipes
 
 import numpy as np
 
-def conductance_short_circular_tube(velocity,A,a=1):
+def conductance_short_circular_tube(velocity,cross_sectional_area,a=1):
     """ The conductance for a short circular tube is equivalent to the conductance of an aperature when a=1
-    :param velocity: avg velocity of the gas, calculated using func in other file
+    :param velocity: typically avg velocity of the gas, can use rms
     :param A: cross-sectional area that the gas passes through
     :param a: transmission probability through the tube
     :return C: conductance of the system in L/s"""
-    C = a*(velocity*A)/4
-    return C*1000
+    conductance = a*(velocity*cross_sectional_area)/4
+    return conductance*1000
 
-def conductance_long_circular_tube(velocity,diameter,l):
+def conductance_long_circular_tube(velocity,diameter,length):
     """
     Conductance of a long circular rounded tube calcuations
     :param velocity: avg boltzmann velocity of a particle
@@ -22,5 +22,5 @@ def conductance_long_circular_tube(velocity,diameter,l):
     :param l: length of the pipe
     :return: C_tube is conductance of the tube in L/s (multiply by 1000 for L)
     """
-    C_tube = (np.pi)*velocity*diameter**3/(12*l)
-    return C_tube*1000
+    c_tube = np.pi*velocity*diameter**3/(12*length)
+    return c_tube*1000
